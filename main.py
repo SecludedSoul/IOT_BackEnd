b550a273c77227232f2d970d66aaa9dd611e4031
pataimas madthai <93992367+SecludedSoul@users.noreply.github.com>
2024-07-31 20:58:23 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],  # อนุญาตทุก HTTP methods
     allow_headers=["*"],  # อนุญาตทุก headers
