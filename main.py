823b809228c5a4837b939d148daea14a4c7b4530
pataimas madthai <93992367+SecludedSoul@users.noreply.github.com>
2024-07-31 22:00:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://iot-front-three.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],  # อนุญาตทุก HTTP methods
     allow_headers=["*"],  # อนุญาตทุก headers
